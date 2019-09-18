6d09328ae35e813ede5444cb29b39a70b584f439
danielsmorais <danielmorais@outlook.com.br>
2019-09-18 15:22:29 -0300

    .

---
+++ exercicio_3/ruidos.m
@@ -0,0 +1,17 @@
+%ATIVIDADE 3
+%DANIEL MORAIS
+%Verificar ruídos nas imagens
+
+image = imread('cristoRedentor/cristo.jpg');
+[linha, coluna] = size(image);
+h = zeros(256,1);
+
+for i=1:linha
+    for j=1:coluna
+        aux = image(i,j);
+        h(aux+1) = h(aux+1) + 1;
+    end
+end
+
+bar(0:255,h); %plot do histograma
+set(gcf, 'Position', [0, 255, 0, max(h)]);
