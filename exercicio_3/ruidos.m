990949f2b123a93a98475e56690e2864ece2db0a
danielsmorais <danielmorais@outlook.com.br>
2019-09-18 16:37:32 -0300

    .

--- exercicio_3/ruidos.m
+++ exercicio_3/ruidos.m
@@ -14,4 +14,7 @@ for i=1:linha
 end
 
 bar(0:255,h); %plot do histograma
-set(gcf, 'Position', [0, 255, 0, max(h)]);
+%set(gcf, 'Position', [0, 255, 0, max(h)]);
+
+set(gca,'xtick',0:255)
+set(gca,'ytick',0:max(h))
