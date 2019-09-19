3c9f3ee3926b083067250d6c6a7ce59e3ce79ab6
Daniel Morais <danielmorais@outlook.com.br>
2019-09-19 13:49:11 -0300

    Fim Exercício 1
    .

--- exercicio_1/laplace.cpp
+++ exercicio_1/laplace.cpp
@@ -7,13 +7,18 @@ using namespace std;
 int main(int argc, char **argv)
 {
     int ncoluna, nlinha;
-    Mat image;
+    Mat image, aguc;
 
     float lap[] = {0,  1,  0,
-                 1,  -4,  1,
-                 0,  1,  0};
+                   1, -4,  1,
+                   0,  1,  0};
 
-    String path = "imagensComRuido/a1.jpg";
+/*     float lap[] = {0, 2, 0,
+                   2, -8, 2,
+                   0, 2, 0}; */
+
+
+    String path = "deserto.png";
 
     image = imread(path, CV_LOAD_IMAGE_GRAYSCALE);
     if (!image.data)
@@ -31,14 +36,20 @@ int main(int argc, char **argv)
     {
         for (int j = 1; j < ncoluna - 1; j++)
         {
-            ilaplace.at<uchar>(i, j) = image.at<uchar>(i - 1, j - 1) * lap[0] / 9 + image.at<uchar>(i - 1, j) * lap[1] / 9 + image.at<uchar>(i - 1, j + 1) * lap[2] / 9 +
-                                       image.at<uchar>(i, j - 1) * lap[3] / 9 + image.at<uchar>(i, j) * lap[4] / 9 + image.at<uchar>(i, j + 1) * lap[5] / 9 +
-                                       image.at<uchar>(i + 1, j - 1) * lap[6] / 9 + image.at<uchar>(i + 1, j) * lap[7] / 9 + image.at<uchar>(i + 1, j + 1) * lap[8] / 9;
+            ilaplace.at<uchar>(i, j) = image.at<uchar>(i - 1, j - 1) * lap[0]/9.0 + image.at<uchar>(i - 1, j)*lap[1]/9.0 + image.at<uchar>(i - 1, j + 1)*lap[2]/9.0 +
+                                       image.at<uchar>(i, j - 1) * lap[3]/9.0 + image.at<uchar>(i, j) * lap[4]/9.0 + image.at<uchar>(i, j + 1)*lap[5]/9.0 +
+                                       image.at<uchar>(i + 1, j - 1)*lap[6]/9.0 + image.at<uchar>(i + 1, j)*lap[7]/9.0 + image.at<uchar>(i + 1, j + 1)*lap[8]/9.0;
         }
     }
 
-    path = "imagensLaplaciano/a1.jpg";
+    aguc = image - ilaplace;
+
+    path = "imagemLaplaciano/deserto.jpg";
     imwrite(path, ilaplace);
-    
+    path = "imagemLaplaciano/deserto_aguc.jpg";
+    imwrite(path, aguc);
+    path = "imagemLaplaciano/deserto_cinza.jpg";
+    imwrite(path, image);
+
     return 0;
 }
